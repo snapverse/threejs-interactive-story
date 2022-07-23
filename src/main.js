431b74c6697f14d8f771d890862bd96236fea6ad
ismithan <ismithanluis@gmail.com>
2022-07-22 23:48:39 -0300

    feat(Root): incline el mapa, chance de map, modifique el clamp de la camara y se instalo camera cont

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import * as THREE from 'three'
 import { MapControls } from 'three/examples/jsm/controls/OrbitControls';
 import { WINDOW_HEIGHT, WINDOW_WIDTH, LIMIT_MAX_X, LIMIT_MIN_X, LIMIT_MAX_Y, LIMIT_MIN_Y } from './constants'
 
+
+
+
 let scene, camera, renderer, controls, listener;
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -13,23 +16,30 @@ document.addEventListener('DOMContentLoaded', () => {
 function init() {
   scene = new THREE.Scene()
   listener = new THREE.AudioListener();
-  camera = new THREE.PerspectiveCamera(79, WINDOW_WIDTH / WINDOW_HEIGHT, 0.1, 1000);
+  camera = new THREE.PerspectiveCamera(70, WINDOW_WIDTH / WINDOW_HEIGHT, 0.1, 1000);
   camera.add(listener)
-  camera.position.set(0, 0, 5);
+  camera.position.set(0, -500, 5000);
 
-  const texture = new THREE.TextureLoader().load('./textures/map4.jpg');
 
-  const geometry = new THREE.BoxGeometry(19, 9, 0);
-  const material = new THREE.MeshBasicMaterial({ map: texture });
-  const cube = new THREE.Mesh(geometry, material);
-  scene.add(cube);
+  const getImageRatioPlane = async () => {
+    const texture = await new THREE.TextureLoader().loadAsync('./textures/map4.jpg');
+    const material = new THREE.MeshBasicMaterial({ map: texture });
+    const geometry = new THREE.PlaneGeometry(texture.image.width, texture.image.height);
+    const plane = new THREE.Mesh(geometry, material);
+    scene.add(plane);
+
+    return geometry
+  }
+  
+  
+  
   
   renderer = new THREE.WebGLRenderer()
   renderer.setPixelRatio(window.devicePixelRatio);
   renderer.setSize(WINDOW_WIDTH, WINDOW_HEIGHT)
   document.body.appendChild(renderer.domElement)
-
-  setMapControls(geometry)
+  
+  setMapControls(getImageRatioPlane())
   addSound(listener, { path: 'sounds/rain.mp3', loop: true, volume: .09 });
 }
 
@@ -67,23 +77,35 @@ function setMapControls(geometry) {
 
   controls.screenSpacePanning = true;
   controls.minDistance = 1;
-  controls.maxDistance = 4;
+  controls.maxDistance = 600;
   controls.enableRotate = false
   controls.enableDamping = true;
   controls.dampingFactor = 0.05;
-  controls.maxPolarAngle = Math.PI / 2;
+  //controls.maxPolarAngle = Math.PI / 2;
   
   var box = new THREE.Box3();
   box.setFromObject(controls.object);
 
-  const minPan = new THREE.Vector3(-2, -2, -2);
-  const maxPan = new THREE.Vector3(2, 2, 2);
-  const _v = new THREE.Vector3();
+  const minPan = new THREE.Vector3(-200, -140, 0);
+  const maxPan = new THREE.Vector3(200, 85, 0);
+  const _v = new THREE.Vector3(2000,2000,1000);
   
   controls.addEventListener('change', evt => {
     _v.copy(controls.target);
     controls.target.clamp(minPan, maxPan);
     _v.sub(controls.target);
     camera.position.sub(_v);
+    console.log(controls.object.position)
   })
+
+  const createSnow = () => {
+    var map = new THREE.TextureLoader().load(snow);
+    var material = new THREE.SpriteMaterial({
+      map: map,
+      transparent: true,
+      opacity: 0.5,
+      color: 0xffffff,
+    });
+    return new THREE.Sprite(material);
+  };
 }
