1e39f19fa184a7ab0bc0c63d21f0525f75be02fc
Simón Villafañe <simonvillafanee@gmail.com>
2022-08-03 01:48:56 -0300

    fix

--- src/main.js
+++ src/main.js
@@ -56,52 +56,26 @@ camera.position.set(0, -90, 593)
   soundsCircle(camera,scene,{ path: 'sounds/tala3.mp3', loop: true, volume:300, distance:0.005, one: 360, two: 120, three: 0.1,play:8},-400,120,100, 300,150,170);
   soundsCircle(camera,scene,{ path: 'sounds/tala4.mp3', loop: true, volume:300, distance:0.005, one: 360, two: 120, three: 0.1,play:12},-400,120,100, 300,150,170);
 
-  soundsCircle(camera,scene,{ path: 'sounds/bosque.mp3',         loop: true, volume: 50 ,distance:0.1, one: 360, two: 220, three: 0.1,play:3}, 20,170,100, 200,100,170);
-  soundsCircle(camera,scene,{ path: 'sounds/bosque2.mp3',        loop: true, volume: 50 ,distance:0.1, one: 360, two: 220, three: 0.1,play:3}, 20,170,80, 200,100,170);
-  soundsCircle(camera,scene,{ path: 'sounds/bosque3.mp3',        loop: true, volume: 50 ,distance:0.1, one: 360, two: 220, three: 0.1,play:3}, 20,170,70, 200,100,170);
-  soundsCircle(camera,scene,{ path: 'sounds/sonidopajaroas.mp3', loop: true, volume: 50 ,distance:0.1, one: 360, two: 220, three: 0.1,play:3}, 20,170,120, 200,100,170);
-  soundsCircle(camera,scene,{ path: 'sounds/alrededores.mp3', loop: true, volume: 20 ,distance:0.1, one: 0, two: 360, three: 0.1}, 60,170,100, 300,150,170);
+  // soundsCircle(camera,scene,{ path: 'sounds/bosque.mp3',         loop: true, volume: 50 ,distance:0.1, one: 360, two: 220, three: 0.1,play:3}, 20,170,100, 200,100,170);
+  // soundsCircle(camera,scene,{ path: 'sounds/bosque2.mp3',        loop: true, volume: 50 ,distance:0.1, one: 360, two: 220, three: 0.1,play:3}, 20,170,80, 200,100,170);
+  // soundsCircle(camera,scene,{ path: 'sounds/bosque3.mp3',        loop: true, volume: 50 ,distance:0.1, one: 360, two: 220, three: 0.1,play:3}, 20,170,70, 200,100,170);
+  // soundsCircle(camera,scene,{ path: 'sounds/sonidopajaroas.mp3', loop: true, volume: 50 ,distance:0.1, one: 360, two: 220, three: 0.1,play:3}, 20,170,120, 200,100,170);
+  // soundsCircle(camera,scene,{ path: 'sounds/alrededores.mp3', loop: true, volume: 20 ,distance:0.1, one: 0, two: 360, three: 0.1}, 60,170,100, 300,150,170);
 
-  soundsCircle(camera,scene,{ path: 'sounds/alrededores.mp3', loop: true, volume: 20 ,distance:0.1, one:360, two: 360, three: 0.1}, 300,300,0, 100,100,100);
+  // soundsCircle(camera,scene,{ path: 'sounds/alrededores.mp3', loop: true, volume: 20 ,distance:0.1, one:360, two: 360, three: 0.1}, 300,300,0, 100,100,100);
 
 
-  soundsCircle(camera,scene,{ path: 'sounds/aldea.mp3', loop: true, volume: 35 ,distance:0.1, one: 360, two: 120, three: 0.1}, -10,-40,100, 150,100,120);
-  soundsCircle(camera,scene,{ path: 'sounds/fogata.mp3', loop: true, volume: 70 ,distance:0.3, one: 120, two: 70, three: 0.1}, -40,-20,20, 10,5,100);
+  // soundsCircle(camera,scene,{ path: 'sounds/aldea.mp3', loop: true, volume: 35 ,distance:0.1, one: 360, two: 120, three: 0.1}, -10,-40,100, 150,100,120);
+  // soundsCircle(camera,scene,{ path: 'sounds/fogata.mp3', loop: true, volume: 70 ,distance:0.3, one: 120, two: 70, three: 0.1}, -40,-20,20, 10,5,100);
 
-  soundsCircle(camera,scene,{ path: 'sounds/mar.mp3', loop: true, volume: 25 ,distance:0.6, one: 0, two: 360, three: 0.4}, 700,-400,0, 10,5,100);
-  soundsCircle(camera,scene,{ path: 'sounds/mar.mp3', loop: true, volume: 10 ,distance:0.3, one: 0, two: 360, three: 0.1}, -800,150,0, 10,5,100);
+  // soundsCircle(camera,scene,{ path: 'sounds/mar.mp3', loop: true, volume: 25 ,distance:0.6, one: 0, two: 360, three: 0.4}, 700,-400,0, 10,5,100);
+  // soundsCircle(camera,scene,{ path: 'sounds/mar.mp3', loop: true, volume: 10 ,distance:0.3, one: 0, two: 360, three: 0.1}, -800,150,0, 10,5,100);
 
-  soundsCircle(camera,scene,{ path: 'sounds/viento.mp3', loop: true, volume: 10 ,distance:0.15, one: 360, two: 260, three: 0.6}, 0,10,550, 300,70,30);
+  // soundsCircle(camera,scene,{ path: 'sounds/viento.mp3', loop: true, volume: 10 ,distance:0.15, one: 360, two: 260, three: 0.6}, 0,10,550, 300,70,30);
 
   //addBackgroundSound(camera, { path: 'sounds/musiquita.mp3', loop: true, volume: .03 });
   
-  musicPlane(camera, { path: 'sounds/musiquita.mp3', loop: true, volume: 11 ,distance:1, one: 0, two: 360, three: 0.2}, 0,0,650);
-
-  function musicPlane(camera, { path, loop, volume,distance,play = 0, one,two,three},transx = 0,transy= 0,transz= 0){
-    const listener = new THREE.AudioListener()
-    camera.add(listener)
-    const sound = new THREE.PositionalAudio( listener );
-    const audioLoader = new THREE.AudioLoader()
-    audioLoader.load(path, function (buffer) {
-      sound.setBuffer(buffer)
-      sound.setLoop(loop)
-      sound.setVolume(volume)
-      sound.setRefDistance (distance) 
-      sound.setDirectionalCone( one, two,three );
-      sound.play(play)
-    })
-    let whidth = 1700,height = 800
-    const geometry = new THREE.PlaneGeometry( whidth, height);
-    const material = new THREE.MeshBasicMaterial({ opacity:1 })
-    material.transparent = true
-    const circleSound = new THREE.Mesh(geometry, material)
-    circleSound.add(sound)
-    
-    circleSound.translateX(transx)
-    circleSound.translateY(transy)
-    circleSound.translateZ(transz)
-    scene.add(circleSound)
-  }
+  // musicPlane(camera, { path: 'sounds/musiquita.mp3', loop: true, volume: 11 ,distance:1, one: 0, two: 360, three: 0.2}, 0,0,650);
 
   document.querySelector('#app')?.appendChild(renderer.domElement)
 
@@ -126,4 +100,30 @@ function animate() {
   renderer.render(scene, camera)
   controls.getTarget(plane.position)
   mmi.update()
+}
+
+function musicPlane(camera, { path, loop, volume,distance,play = 0, one,two,three},transx = 0,transy= 0,transz= 0){
+  const listener = new THREE.AudioListener()
+  camera.add(listener)
+  const sound = new THREE.PositionalAudio( listener );
+  const audioLoader = new THREE.AudioLoader()
+  audioLoader.load(path, function (buffer) {
+    sound.setBuffer(buffer)
+    sound.setLoop(loop)
+    sound.setVolume(volume)
+    sound.setRefDistance (distance) 
+    sound.setDirectionalCone( one, two,three );
+    sound.play(play)
+  })
+  let whidth = 1700,height = 800
+  const geometry = new THREE.PlaneGeometry( whidth, height);
+  const material = new THREE.MeshBasicMaterial({ opacity:1 })
+  material.transparent = true
+  const circleSound = new THREE.Mesh(geometry, material)
+  circleSound.add(sound)
+  
+  circleSound.translateX(transx)
+  circleSound.translateY(transy)
+  circleSound.translateZ(transz)
+  scene.add(circleSound)
 }
